d43293c47ccaaa42ac0e58e51a1e0ef703340cbd
Kaung Set <121620228+erke31-2@users.noreply.github.com>
2023-06-01 18:07:31 +0630

    some changes App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "countries/:name",
+        path: "countries/:code",
         element: <Details />,
       },
     ],
